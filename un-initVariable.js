b0ed9222940b6738f6a6fe84eb9f9a84f9068581
Marcus Galvin <marcusgalvin@Marcuss-MBP-2.fios-router.home>
2019-07-02 10:23:52 -0400

    un-Init Variables in JS

---
+++ un-initVariable.js
@@ -0,0 +1,30 @@
+//Directions:
+
+// Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
+
+//Given Code;
+
+// Initialize these three variables
+var a
+var b
+var c 
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+
+//Solution:
+
+// Initialize these three variables
+var a = 5
+var b = 10
+var c ="I am a"
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
